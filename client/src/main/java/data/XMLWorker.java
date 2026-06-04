3715858abc044e1376473d5e94e38f80223f3764
Ignat <boyIgnat@gmail.com>
2026-06-05 01:02:58 +0300

    Улучшение XMLWorker.

--- client/src/main/java/data/XMLWorker.java
+++ client/src/main/java/data/XMLWorker.java
@@ -1,13 +1,17 @@
 package data;
 
 import com.fasterxml.jackson.core.JacksonException;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class XMLWorker {
+
     private static final XmlMapper mapper = new XmlMapper();
 
-    public static String serialize(Object object) throws JacksonException {
+    static {
+        mapper.findAndRegisterModules();
+    }
+
+    public static <T> String serialize(T object) throws JacksonException {
         return mapper.writeValueAsString(object);
     }
 
