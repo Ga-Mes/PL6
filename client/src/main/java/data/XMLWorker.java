80b4b764e661feeab4e83bff3bb7c586d0ee9f7d
Ignat <boyIgnat@gmail.com>
2026-06-05 01:01:22 +0300

    Добавление десериализации.

--- client/src/main/java/data/XMLWorker.java
+++ client/src/main/java/data/XMLWorker.java
@@ -1,12 +1,17 @@
 package data;
 
+import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class XMLWorker {
     private static final XmlMapper mapper = new XmlMapper();
 
-    public static String serialize(Object object) throws JsonProcessingException {
+    public static String serialize(Object object) throws JacksonException {
         return mapper.writeValueAsString(object);
+    }
+
+    public static <T> T parse(String xml, Class<T> clazz) throws JacksonException {
+        return mapper.readValue(xml, clazz);
     }
 }
