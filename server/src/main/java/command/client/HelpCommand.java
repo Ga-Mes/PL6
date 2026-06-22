962cf84a5d11a67516f0613c25d5ed48f348ecad
Ignat <boyIgnat@gmail.com>
2026-06-22 19:46:12 +0300

    Дополнение команды help.

--- server/src/main/java/command/client/HelpCommand.java
+++ server/src/main/java/command/client/HelpCommand.java
@@ -15,7 +15,21 @@ public class HelpCommand extends AbstractClientCommand {
         Response response;
 
         if (Lexer.transform(request) != null) {
-            response = new Response(1, "List of commands...");
+            response = new Response(1, """
+                help - display a list of available commands.
+                info - output information about the collection.
+                show - display all elements of the collection in string representation.
+                insert key name age description - add a new element with the specified key.
+                update id name age description - update the value of the collection element with the specified id.
+                remove_key key - remove an element from the collection by its key.
+                clear - clear the collection.
+                exit - terminate the program.
+                remove_greater name age description - remove all elements from the collection that are greater than the specified one.
+                remove_greater_key key - remove all elements from the collection whose key exceeds the specified one.
+                filter_contains_description description - display elements whose description field contains the given substring.
+                filter_greater_than_age age - display elements whose age field is greater than the specified value.
+                print_unique_color - display the unique values of the color field for all elements in the collection."""
+            );
         } else {
             response = new Response(2, "Wrong primitive arguments...");
         }
