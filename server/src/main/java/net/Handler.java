e65e25b40e2a1804b92cb5c2292b42d348d13f54
Ignat <boyIgnat@gmail.com>
2026-06-22 10:06:01 +0300

    Получение ответа от команды.

--- server/src/main/java/net/Handler.java
+++ server/src/main/java/net/Handler.java
@@ -1,5 +1,7 @@
 package net;
 
+import command.AbstractClientCommand;
+import command.client.HelpCommand;
 import data.CollectionManager;
 import data.XMLWorker;
 import org.slf4j.Logger;
@@ -28,6 +30,8 @@ public class Handler {
     }
 
     private final Map<UUID, TreeMap<Integer, byte[]>> toAssemble = new HashMap<>();
+
+    private final Map<UUID, RequestContext> contexts = new HashMap<>();
 
     public void tick(boolean[] statuses, Logger logger, CollectionManager collectionManager) {
         ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE * 2);
@@ -70,7 +74,32 @@ public class Handler {
                             Request.class
                     );
 
-                    logger.info(String.valueOf(request));
+                    if (!contexts.containsKey(uuid)) {
+                        AbstractClientCommand command = switch (request.type()) {
+                            case HELP -> new HelpCommand();
+                            case INFO -> null;
+                            case SHOW -> null;
+                            case INSERT -> null;
+                            case UPDATE -> null;
+                            case REMOVE_KEY -> null;
+                            case CLEAR -> null;
+                            case EXECUTE_SCRIPT -> null;
+                            case EXIT -> null;
+                            case REMOVE_GREATER -> null;
+                            case REPLACE_IF_GREATER -> null;
+                            case REMOVE_GREATER_KEY -> null;
+                            case FILTER_CONTAINS_DESCRIPTION -> null;
+                            case FILTER_GREATER_THAN_AGE -> null;
+                            case PRINT_UNIQUE_COLOR -> null;
+                            case SAVE -> null;
+                        };
+
+                        contexts.put(uuid, new RequestContext(command));
+                    }
+
+                    Response response = contexts.get(uuid).handle(statuses, logger, collectionManager, request);
+
+                    logger.info(String.valueOf(response));
                 }
 
                 ByteBuffer aBuffer = ByteBuffer.allocate(Long.BYTES * 2 + Integer.BYTES * 3);
