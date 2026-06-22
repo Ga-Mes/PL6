c5e8fb020523723f4bd8ae8b1fa7761909c307e6
Ignat <boyIgnat@gmail.com>
2026-06-22 15:54:06 +0300

    Формирователь дракона из шаблона и примитивов.

--- server/src/main/java/base/DragonChecker.java
+++ server/src/main/java/base/DragonChecker.java
@@ -5,7 +5,7 @@ import data.CollectionManager;
 import java.util.ArrayList;
 
 public class DragonChecker {
-    public Dragon form(DragonTemplate template, ArrayList<Object> primitives, int i, CollectionManager manager) {
+    public static Dragon form(DragonTemplate template, ArrayList<Object> primitives, int i, CollectionManager manager) {
         return new Dragon((String) primitives.get(i), template.getCoordinates(), (Integer) primitives.get(i++), (String) primitives.get(i), template.getColor(), template.getCharacter(), template.getCave(), manager.dragons);
     }
 }
